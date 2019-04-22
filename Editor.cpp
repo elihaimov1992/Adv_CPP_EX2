b83cb0f66de5609a6ca64939b0cef1ebce1c950d
elihaimov1992 <37331405+elihaimov1992@users.noreply.github.com>
2019-04-22 20:36:49 +0300

    Update Editor.cpp

--- Editor.cpp
+++ Editor.cpp
@@ -1,22 +1,21 @@
 /*Eli Haimov - 308019306*/
 
+
 #include "Editor.h"
 
-
+// C'tor
 Editor::Editor()
 {
     document = new Document();
 }
 
+// loop function
 void Editor::loop()
 {
      string str_in; // String Input
     
-    
-
     while(true)
     {
-        // Source of getline: www.cplusplus.com/reference/string/string/getline/
        getline(cin,str_in);
        if(str_in == "p")
        {
@@ -34,13 +33,12 @@ void Editor::loop()
        }
 
        // For make_curr_line function(Command Meaning):
-       // Source 1: www.cplusplus.com/reference/cstdlib/atoi/
        // Source 2: www.cplusplus.com/reference/sstream/istringstream/istringstream/
        else if(atoi(str_in.c_str()))
        {
            int num;
-           istringstream ss(str_in);
-           ss>>num;
+           istringstream iss(str_in);
+           iss>>num;
            document->make_curr_line(num);
        }
     
@@ -71,17 +69,16 @@ void Editor::loop()
 
        else if(str_in.at(0) == 's')
        {
-           if(str_in.substr(2 , str_in.size()).find('/') == string::npos)
+           if(str_in.substr(2 , str_in.size()).find('/') != string::npos)
            {
-               cerr << "The Document haven't lines" << endl;
-               break; 
+               int ind = str_in.substr(2 , str_in.size()).find('/');  // ind = Index
+               int ind_3 = (ind + 3); 
+               int ind_end_4 = (str_in.size() - (ind + 4));
+               document->replace_old_new(str_in.substr(2 , ind) , str_in.substr(ind_3 , ind_end_4));
            }
            else
            {
-               int ind = str_in.substr(2 , str_in.size()).find('/');  // ind = Index
-               int ind_3 = ind + 3; 
-               int ind_end_4 = str_in.size() - (ind + 4);
-               document->replace_old_new(str_in.substr(2 , ind) , str_in.substr(ind_3 , ind_end_4));
+               cerr << "Old word have'nt exist" << endl; 
            }
        }
 
@@ -93,6 +90,7 @@ void Editor::loop()
     }
 }
 
+// D'tor
 Editor::~Editor()
 {
     delete document;
